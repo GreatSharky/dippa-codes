3633e3dfe96345185d2ee54dff873a4c82d2eee4
Aaro Haikonen <aaro.haikonen99@gmail.com>
2025-02-23 19:34:46 +0200

    updated ollama_agent.py

--- rock-paper-scissors/ollama_agent.py
+++ rock-paper-scissors/ollama_agent.py
@@ -4,18 +4,18 @@ import cv2
 import json
 import ollama
 
-image = cv2.imread("image.png")
-result, jpeg = cv2.imencode(".jpg", image)
+jpeg = cv2.imread("s.jpg")
+res, jpeg =cv2.imencode(".jpg", jpeg)
 jpg_text = base64.b64encode(jpeg).decode("utf-8")
-
 response = ollama.chat(model="llama3.2-vision", messages=[
     {
         "role" : "system",
-        "content" : "You are rock-paper-scissors(RPS) judge. You need to answer what RPS object the hand in the image is representing. Answer using json."
+        "content" : "You are rock-paper-scissors(RPS) judge. You need to answer what RPS object the hand in the image is representing. Respond using JSON",
+        "format" : "json",
     },
     {
         "role" : "user",
-        "conent" : "Which RPS choice is in the image. Asnswer using json.",
+        "conent" : "Which RPS choice is in the image? Answer only using JSON.",
         "format" : "json",
         "images" : [jpg_text]
     }
